8c0ded530e993fa94f1c022029f44a8f68e9e3f2
Mahabubul Hasan Masud <mhasan@granburyrs.com>
2015-09-23 14:30:34 -0500

    boom is done

---
+++ jsx/boom.jsx
@@ -0,0 +1,35 @@
+var Boom = React.createClass({
+
+	getInitialState: function(){
+		return {
+			timeRemaining: 10
+		}
+	},
+
+	countDown: function(){
+		var actualRemainingTime = this.state.timeRemaining - 1;
+
+		if (actualRemainingTime == 0) {
+			this.setState({timeRemaining : 'Boom!'});
+			clearInterval(this.intervalId);
+		}else{
+			this.setState({timeRemaining : actualRemainingTime});
+		}
+	},
+
+	componentDidMount: function(){
+
+		this.intervalId = setInterval(this.countDown, 1000);
+	},
+
+	render: function() {
+		return (
+			<div>
+				<h1>{this.state.timeRemaining}</h1>
+			</div>
+		);
+	}
+
+});
+
+React.render(<Boom />, document.body);
